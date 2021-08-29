7b14c9bbfe84909fabeed8b306fed5dbf5d3dba8
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2021-08-28 21:10:34 -0500

    Update and rename index.js to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import apiRouter from './src/routes'
+import apiRouter from './routes'
 
 const app = express()
 const port = process.env.PORT || 3000
